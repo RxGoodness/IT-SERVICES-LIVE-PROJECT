404b1455be6a6f5b2be7f469124b55cf359548d7
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-05-09 19:23:02 +0100

    Created admin route

---
+++ server/src/routes/admin.route.ts
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router();
+import { createAdmin, loginAdmin } from "../controllers/admin.controller";
+
+router.route("/create").post(createAdmin);
+router.route("/login").post(loginAdmin);
+
+export default router;
