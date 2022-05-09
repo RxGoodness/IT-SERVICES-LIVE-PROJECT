5e226e8ce18cd33aa496fc151b11733db1c60a6e
Decagon <decagon@Alexander.local>
2022-05-10 00:48:53 +0100

    Minor Update

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,14 +4,14 @@ import editProfileValidator from '../middlewares/index';
 import {createJob, deleteCreatedJob, updateCreatedJob} from '../controllers/jobController';
 import  { createProject, updateProject, deleteProject}from '../controllers/project';
 import uploadOptions from '../middlewares/imageUpload';
-import {projectValidatior} from "../middlewares/projectMiddleware"
+import {projectValidatior} from "../middlewares/projectMiddleware";
 const router = express.Router();
 
 
 /* GET home page. */
 
-router.post("/projects", projectValidatior, createProject)
-router.put("/projects/:id", projectValidatior, updateProject)
+router.post("/projects", projectValidatior, createProject);
+router.put("/projects/:id", projectValidatior, updateProject);
 router.delete("/projects/:id", deleteProject);
 router.post('/create_jobs', uploadOptions.single('image'), createJob);
 router.put('/update_jobs/:id', uploadOptions.single('image'), updateCreatedJob);
