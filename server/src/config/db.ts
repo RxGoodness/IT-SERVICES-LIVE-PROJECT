e6071d678617b55fafec9d1d779ae8aa442a0133
Ugochi Iwuchukwu <iwuchukwuugochij@gmail.com>
2022-05-11 13:47:45 +0100

    completed quote task

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI as string);
+    const conn = await mongoose.connect(process.env.NODE_ENV! === "test" ? process.env.TEST_MONGO_URI! : process.env.MONGO_URI!);
     console.log(`🔌 Database connected to ${conn.connection.host}`);
   } catch (err) {
     console.log(`could not connect to mongodb ---- ${err}`);
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -18,4 +18,5 @@ router.get("/jobs", getAllJobs)
 
 
 
+
 export default router;
