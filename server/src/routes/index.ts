74545b4a7e90ee47d36c118e180f8af5a867551a
adefoluso <adedejifoluso@gmail.com>
2022-05-11 15:04:57 +0100

    emailservice

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,6 +3,8 @@ import {createJob, deleteCreatedJob, updateCreatedJob} from '../controllers/jobC
 import  { createProject, updateProject, deleteProject}from '../controllers/project';
 import uploadOptions from '../middlewares/imageUpload';
 import {projectValidatior} from "../middlewares/projectMiddleware"
+import {contactMessage, deleteNotification, viewNotifications} from "../controllers/NotificationControllers"
+import {sendMail} from "../controllers/messageContacts"
 const router = express.Router();
 
 /* GET home page. */
@@ -13,6 +15,10 @@ router.delete("/projects/:id", deleteProject);
 router.post('/create_jobs', uploadOptions.single('image'), createJob);
 router.put('/update_jobs/:id', uploadOptions.single('image'), updateCreatedJob);
 router.delete('/delete_jobs/:id', deleteCreatedJob);
+router.post('/contact_us', contactMessage)
+router.delete('/delete_notification/:id', deleteNotification)
+router.get('/view_notifications', viewNotifications)
+router.post('/send_mail', sendMail)
 
 
 export default router;
