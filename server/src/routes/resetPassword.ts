24741333726f9c8dda61cb8af0f31e53817c850c
Gboyega Lawal <afiz.lawal51@gmail.com>
2022-05-11 09:56:23 +0100

    conflict fixed?

--- server/src/routes/resetPassword.ts
+++ server/src/routes/resetPassword.ts
@@ -2,7 +2,7 @@ import express from "express";
 const router = express();
 import { enterEmail, resetPassword } from "../controllers/resetPassword";
 
-router.post("/", enterEmail);
+router.post("/enter-email", enterEmail);
 router.post("/:userId/:token", resetPassword);
 
 export default router;
