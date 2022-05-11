947d6f09c6b3c7e4779b276ee5fc69cf6e82fde0
rxgoodness <adedokuntoyyib@gmail.com>
2022-05-11 14:02:11 +0100

    created blog routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import logger from "morgan";
 import { connectDB } from "./config";
 
 import router from "./routes";
+import blogRouter from "./routes/blogRoute";
 const cors = require("cors");
 
 const app = express();
@@ -12,9 +13,10 @@ app.options("*", cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/public', express.static('public'))
+app.use("/public", express.static("public"));
 
 app.use("/", router);
+app.use("/", blogRouter);
 
 connectDB();
 
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,6 +3,7 @@ import {createJob, deleteCreatedJob, updateCreatedJob} from '../controllers/jobC
 import  { createProject, updateProject, deleteProject}from '../controllers/project';
 import uploadOptions from '../middlewares/imageUpload';
 import {projectValidatior} from "../middlewares/projectMiddleware"
+import { createBlog, editBlog, deleteBlog, viewBlog, viewBlogs, commentPost} from '../controllers/blogPost';
 const router = express.Router();
 
 /* GET home page. */
@@ -15,4 +16,10 @@ router.put('/update_jobs/:id', uploadOptions.single('image'), updateCreatedJob);
 router.delete('/delete_jobs/:id', deleteCreatedJob);
 
 
+router.post("/blog", createBlog)
+router.put("/blog/:id", editBlog)
+router.delete("/blog/:id", deleteBlog)
+router.get("/blog/:id", viewBlog)
+router.get("/blog", viewBlogs)
+router.post("/blog/comment/:id/", commentPost);
 export default router;
