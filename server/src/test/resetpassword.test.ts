899ca040e7ff39b46cfbdce6d3ce20ca33586373
Gboyega Lawal <afiz.lawal51@gmail.com>
2022-05-14 16:56:37 +0100

    updates

--- server/src/test/resetpassword.test.ts
+++ server/src/test/resetpassword.test.ts
@@ -10,6 +10,17 @@ describe("enter emil for password reset", () => {
       })
       .expect(400);
 
-    console.log(response);
+    // console.log(response);
+  });
+
+  it("change password", async () => {
+    const response = await request(app)
+      .post("/reset-password/627b77a28fdb119e19269ec8/123456")
+      .send({
+        password: "majorasMASK@1",
+        reEnterPassword: "majorasMASK@1",
+      })
+      .expect(200);
+    console.log(response.body);
   });
 });
