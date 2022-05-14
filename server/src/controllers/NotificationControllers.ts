35b7e22814d18d38608d2e1eb7849295c86ef807
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-05-14 12:09:54 +0100

    Implemented Error Handler

--- server/src/controllers/NotificationControllers.ts
+++ server/src/controllers/NotificationControllers.ts
@@ -4,7 +4,7 @@ import asyncHandler from "express-async-handler";
 import ContactMessages from '../models/contactFormSchema'
 
 const contactMessage = asyncHandler(
-    async (req: Request, res: Response): Promise<any> => {
+    async (req: Request, res: Response) => {
         const schema = Joi.object({
             name: Joi.string().required(),
             email: Joi.string().email().required(),
@@ -24,7 +24,7 @@ const contactMessage = asyncHandler(
 
             await ContactMessages.create(postedMessage);
 
-            return res.status(200).json({
+            res.status(200).json({
                 message: "Thank you for contacting us"
                 // data
             })
@@ -37,27 +37,33 @@ const contactMessage = asyncHandler(
 )
 
 
-const deleteNotification = async (req: Request, res: Response) => {
-    console.log(req.params.id)
-    const deletedNotification = await ContactMessages.findByIdAndRemove({ _id: req.params.id })
+const deleteNotification = asyncHandler(
+    async (req: Request, res: Response) => {
+        console.log(req.params.id)
+        const deletedNotification = await ContactMessages.findByIdAndRemove({ _id: req.params.id })
 
-    if (!deletedNotification) {
-        res.status(400);
-        throw new Error("Message not found in database")
-    } else {
-        return res.status(200).json({ message: "Notification deleted Successfully" })
+        if (!deletedNotification) {
+            res.status(400);
+            throw new Error("Message not found in database")
+        } else {
+            res.status(200).json({ message: "Notification deleted Successfully" })
+        }
     }
-}
+)
 
-const viewNotifications = async (req: Request, res: Response) => {
-    try {
-        const params = req.query;
-        const notifications = await ContactMessages.find(params)
-        return res.status(200).json(notifications)
-    } catch (error) {
-        res.status(400)
-        throw new Error("Message not found")
+
+const viewNotifications = asyncHandler(
+    async (req: Request, res: Response) => {
+        try {
+            const params = req.query;
+            const notifications = await ContactMessages.find(params)
+            res.status(200).json(notifications)
+        } catch (error) {
+            res.status(400)
+            throw new Error("Message not found")
+        }
     }
-}
+)
+
 
 export { contactMessage, deleteNotification, viewNotifications }
