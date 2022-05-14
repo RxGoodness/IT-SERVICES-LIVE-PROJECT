d9aa2960426fea288634f9c69ff3352ca10a7bb4
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-05-14 08:44:43 +0100

    used ES6 export syntax

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,37 +1,44 @@
-import express from "express";
+import cors from "cors";
 import logger from "morgan";
+import express from "express";
 import { connectDB } from "./config/db";
-import {notFoundMiddleware} from "./middlewares/errorMiddlewares/notFound"
-import editViewRouter from "./routes/editViewUser";
-import projectRoute from "./routes/project.route";
-import notificationRoute from "./routes/notification.route";
-import jobRoute from "./routes/jobs.route";
-
-import blogRouter from "./routes/blogRoute";
+// Routes
 import admin from "./routes/admin.route";
-import resetPasswordRoute from "./routes/resetPassword";
+import jobRoute from "./routes/jobs.route";
+import blogRouter from "./routes/blogRoute";
 import quoteRoute from "./routes/quoteRoute";
-const cors = require("cors");
+import projectRoute from "./routes/project.route";
+import editViewRouter from "./routes/editViewUser";
+import resetPasswordRoute from "./routes/resetPassword";
+import notificationRoute from "./routes/notification.route";
+// Error handlers
+import notFound from "./middlewares/errorMiddlewares/notFound";
+import errorHandler from "./middlewares/errorMiddlewares/errorHandler";
 
 const app = express();
 
+// Middlewares
 app.use(cors());
-app.options("*", cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/public", express.static("public"));
 
-app.use("/profile", editViewRouter);
-app.use("/projects", projectRoute);
-app.use("/", notificationRoute);
+// Routes controllers
+app.use("/admin", admin);
 app.use("/jobs", jobRoute);
 app.use("/blog", blogRouter);
-app.use("/admin", admin);
-app.use("/reset-password", resetPasswordRoute);
 app.use("/quotes", quoteRoute);
+app.use("/", notificationRoute);
+app.use("/projects", projectRoute);
+app.use("/profile", editViewRouter);
+app.use("/reset-password", resetPasswordRoute);
 
-app.use(notFoundMiddleware)
+// Error handlers
+app.use(notFound);
+app.use(errorHandler);
+
+// Connect database
 connectDB();
 
-module.exports = app;
+export default app;
