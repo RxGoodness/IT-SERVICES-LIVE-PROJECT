f47d1dd74514b76cae4da54d047d500c6cdc189e
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-05-14 12:12:48 +0100

    Implemented Error Handler

--- server/src/controllers/faq-controller.ts
+++ server/src/controllers/faq-controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import faqModel from "../models/faq-models";
+import asyncHandler from "express-async-handler";
+
 import Joi from 'joi'
 
 export const allFaqs = async (_req: Request, res: Response) => {
   let allFaqs = await faqModel.find({})
-  res.send(allFaqs);
+  res.status(200).json(allFaqs);
 };
 
 const schema = Joi.object({
@@ -14,60 +16,65 @@ const schema = Joi.object({
 })
 
 
-export const createFaqs = async (req: Request, res: Response) => {
-  try {
+export const createFaqs = asyncHandler(
+  async (req: Request, res: Response) => {
+    try {
 
-    const {questions, answers} = req.body
-  let {error} = schema.validate(req.body)
-  if(error) {
-    return res.status(400).json({error: error.details[0].message})
+      const { questions, answers } = req.body
+      await schema.validateAsync(req.body);
+
+      // console.log("this is it: ",authenticatedInput)
+      let newEntry = new faqModel({
+        questions,
+        answers
+      });
+
+      await newEntry.save()
+
+      res.status(201).json({ newEntry })
+
+    } catch (error) {
+      res.status(404);
+      throw new Error("Question couldn't be added at this time")
+    }
+
   }
-  // console.log("this is it: ",authenticatedInput)
-  let newEntry =  new faqModel({
-    questions,
-    answers
-  });
+)
 
-  await newEntry.save()
 
-  return res.status(201).json({newEntry})
 
-  } catch (error) {
-      res.status(404).json({message: "Question couldn't be added at this time"})
+export const updateFaq = asyncHandler(
+  async (req: Request, res: Response) => {
+    try {
+      const questionID = req.params.id;
+
+
+      const { questions, answers } = req.body;
+      const faq = await faqModel.findOneAndUpdate(
+        { id: questionID },
+        { questions: questions, answers: answers }
+      );
+
+      const updatedFaq = await faqModel.find({ _id: faq.id })
+      res.status(200).json({ updatedFaq });
+    } catch (error) {
+      res.status(404);
+      throw new Error("Requested ID not found");
+    }
   }
-  
-};
+)
 
 
-export const updateFaq = async (req: Request, res: Response) => {
+export const deleteFaq = async (req: Request, res: Response) => {
   try {
     const questionID = req.params.id;
 
-    
-     const { questions, answers} = req.body;
-    const faq = await faqModel.findOneAndUpdate(
-      { id: questionID },
-      { questions: questions, answers: answers }
-    );
-
-    const updatedFaq = await faqModel.find({_id:faq.id})
-    res.status(200).json({ updatedFaq });
+    const deleted = await faqModel.findByIdAndDelete({ _id: questionID })
+    deleted.deletedCount
+      ? res.status(200).json({ message: "Deleted successfully" })
+      : res.status(404).json({ message: "Already deleted" });
   } catch (error) {
-    res.status(404).json({ message: "Requested ID not found" });
+    res.status(404);
+    throw new Error("Requested ID not found");
   }
 };
-
-export const deleteFaq = async (req: Request, res: Response) => {
-    try {
-      const questionID = req.params.id;
-     
-      const deleted = await faqModel.findByIdAndDelete({_id: questionID})
-      deleted.deletedCount
-        ? res.status(200).json({ message: "Deleted successfully" })
-        : res.status(404).json({ message: "Already deleted" });
-    } catch (error) {
-      res.status(404).json({ message: "Requested ID not found" });
-    }
-  };
-
-
