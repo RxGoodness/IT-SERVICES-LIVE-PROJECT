39a3708fc88fa1360a7a48dcb34cef716320a5b0
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-05-16 22:02:02 +0100

    Created cloudinary image middleware

---
+++ server/src/middlewares/process.image.ts
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: async (_req, _file) => {
+    return {
+      folder: "DEV",
+      allowed_formats: ["png", "jpeg", "jpg"],
+    };
+  },
+});
+
+//Function to delete image if any error
+export const deleteImg = async (id: string) => {
+  return cloudinary.uploader.destroy(id);
+};
+
+export default multer({ storage });
