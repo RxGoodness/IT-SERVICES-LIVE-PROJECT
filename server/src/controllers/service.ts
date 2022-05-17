f460cef5ebf110e39251b2b67b587200ec750a17
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-05-17 22:29:08 +0100

    service controller

--- server/src/controllers/service.ts
+++ server/src/controllers/service.ts
@@ -1,102 +1,111 @@
 import { Request, Response } from 'express';
 import Service from '../models/service';
+import asyncHandler from 'express-async-handler';
 
-export const createService = async (
-  req: Record<string, any>,
-  res: Response
-) => {
-  const {
-    category,
-    subDescription,
-    image,
-    description,
-    bestFeature,
-    technologies,
-    pricePackages,
-  } = req.body;
-  const file = req.file;
-
-  if (!file) {
-    res.status(400);
-    throw new Error('No image in the request');
-  }
-
-  try {
-    const fileName = req.file.filename;
-    const basePath = `${req.protocol}://${req.get(
-      'host'
-    )}/public/uploads/${fileName}`;
-    const newService = await Service.create({
+export const createService = asyncHandler(
+  async (req: Record<string, any>, res: Response) => {
+    const {
       category,
-      subcategory: [
-        {
-          subDescription,
-        },
-        {
-          image: basePath ? image : null,
-        },
-      ],
+      subDescription,
       description,
       bestFeature,
       technologies,
       pricePackages,
+    } = req.body;
+
+    const files = req.files;
+
+    if (!files) {
+      res.status(400);
+      throw new Error('No image in the request, Please choose files');
+    }
+    console.log(files);
+
+    // try {
+    const filesPath = files.map((file: any) => {
+      return `${req.protocol}://${req.get('host')}/public/uploads/${
+        file.filename
+      }`;
     });
-    return res.status(201).json(newService);
-  } catch (error) {
-    res.status(400);
-    throw new Error('Service not created');
-  }
-};
+    console.log(filesPath);
 
-export const updateService = async (
-  req: Record<string, any>,
-  res: Response
-) => {
-  const { category, description, bestFeature, technologies, pricePackages } =
-    req.body;
-  const ownerId = req.params.id;
-  const file = req.file;
-  let imagepath: any;
-
-  const service = await Service.findById(ownerId);
-
-  if (file) {
-    const fileName = file.filename;
-    const basePath = `${req.protocol}://${req.get(
-      'host'
-    )}/public/uploads/${fileName}`;
-    imagepath = basePath;
-  } else {
-    imagepath = service.image;
-  }
-
-  try {
-    await Service.findByIdAndUpdate(
-      service,
-      {
-        category,
-        subcategory: [
-          {
-            description,
-          },
-          {
-            image: imagepath,
-          },
-        ],
-        description,
-        bestFeature,
-        technologies,
-        pricePackages,
+    const service = {
+      category,
+      subcategory: {
+        subDescription: Array.isArray(subDescription)
+          ? [subDescription]
+          : subDescription,
+        images: filesPath,
       },
-      { new: true }
+      description,
+      bestFeature,
+      technologies,
+      pricePackages,
+    };
+
+    const newService = await Service.create(service);
+
+    res.status(201).json(newService);
+  }
+);
+
+export const updateService = asyncHandler(
+  async (req: Record<string, any>, res: Response) => {
+    const { category, description, bestFeature, technologies, pricePackages } =
+      req.body;
+
+    const ownerId = req.params.id;
+    const files = req.files;
+
+    let imagepath: any;
+
+    const service = await Service.findById(ownerId);
+
+    if (!service) {
+      res.status(400).json({ msg: 'Service not found' });
+    }
+    let filesPath;
+    if (files) {
+      filesPath = files.map((file: any) => {
+        return `${req.protocol}://${req.get('host')}/public/uploads/${
+          file.filename
+        }`;
+      });
+    }
+
+    await Service.updateOne(
+      { id: ownerId },
+      {
+        ...req.body,
+        ...service.subcategory,
+        subcategory: {
+          subDescription: req.body.subDescription,
+          images: filesPath,
+        },
+      }
     );
+    // imagepath = filesPath;
+    // } else {
+    //   imagepath = service.images;
+    // }
+    // console.log('here', service);
+
+    // const newUpdate = {
+    //   category,
+    //   subcategory: {
+    //     subDescription,
+    //     images: imagepath,
+    //   },
+    //   description,
+    //   bestFeature,
+    //   technologies,
+    //   pricePackages,
+    // };
+    // await Service.findByIdAndUpdate(ownerId, [...service], newUpdate);
 
     res.status(200).json({ msg: 'Service updated successfully' });
-  } catch (error) {
-    res.status(400);
-    throw new Error('Service not updated');
   }
-};
+);
 
 export const deleteService = async (req: Request, res: Response) => {
   const ownerId = req.params.id;
