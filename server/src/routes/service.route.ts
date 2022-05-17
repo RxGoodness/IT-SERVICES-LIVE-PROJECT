4c565ffd1cbad1055b01358e3b05727631ad445a
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-05-17 22:30:34 +0100

    service route

--- server/src/routes/service.route.ts
+++ server/src/routes/service.route.ts
@@ -2,12 +2,11 @@ import express from "express";
 
 import { validateService } from "../middlewares/serviceMiddleware";
 import { createService, updateService, deleteService } from "../controllers/service";
-import uploadOptions from "../middlewares/imageUpload";
-
+import uploadOptions from '../middlewares/serviceImagesUploader';
 const router = express.Router();
 
-router.post("/", uploadOptions.single('image'),  validateService, createService);
-router.put("/:id", uploadOptions.single('image'), validateService, updateService);
+router.post("/", uploadOptions.array('images'), validateService, createService);
+router.put("/:id", uploadOptions.array('images'), validateService, updateService);
 router.delete("/:id", deleteService);
 
 export default router;
